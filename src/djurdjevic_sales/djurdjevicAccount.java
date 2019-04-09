95e05d90e4747833b27ca3fdab6a79c7e922bf5c
djurdjevic <dju18529@spengergasse.at>
2019-04-09 19:44:09 +0200

    add double credit

--- src/djurdjevic_sales/djurdjevicAccount.java
+++ src/djurdjevic_sales/djurdjevicAccount.java
@@ -2,4 +2,25 @@ package djurdjevic_sales;
 
 public class djurdjevicAccount {
 
+	private int customerID;
+	private String firstName;
+	
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
 }
